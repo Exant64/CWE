021578316da7c4d8a9498175e3b92acf5540316f
Exant64 <warpcauldronproject@gmail.com>
2024-03-04 20:00:23 +0100

    disable_reincarnation: kill natural lifespan decreasing

--- CWE/code_system/codes/include/easy/code_disable_reincarnation.h
+++ CWE/code_system/codes/include/easy/code_disable_reincarnation.h
@@ -6,4 +6,8 @@ PATCH_CODE(DisableReincarnation);
 void DisableReincarnation::Init() {
 	// kills "AL_CalcIntentionScore_Dead"
 	WriteJump((void*)0x00562A20, nullsub_1);
+
+	// kills Lifespan sets in "AL_CalcParameter"
+	WriteData<7>((char*)0x005354B2, (char)0x90);
+	WriteData<7>((char*)0x005354C9, (char)0x90);
 }
