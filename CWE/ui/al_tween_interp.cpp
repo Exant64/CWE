36e37682d37e1e63973fc271feb4f31bc810d52f
Exant64 <warpcauldronproject@gmail.com>
2024-05-22 01:53:09 +0200

    Added Elastic interpolation to tween

--- CWE/ui/al_tween_interp.cpp
+++ CWE/ui/al_tween_interp.cpp
@@ -83,10 +83,42 @@ static float QuadEaseInOut(float t, float b, float c, float d) {
 	return -c / 2 * (((t - 2) * (t - 1)) - 1) + b; // ???
 }
 
+static float ElasticEaseIn(float t, float b, float c, float d) {
+	if (t == 0) return b;  if ((t /= d) == 1) return b + c;
+	float p = d * .3f;
+	float a = c;
+	float s = p / 4;
+	float postFix = a * pow(2, 10 * (t -= 1)); // this is a fix, again, with post-increment operators
+	return -(postFix * sin((t * d - s) * (2 * M_PI) / p)) + b;
+}
+
+float ElasticEaseOut(float t, float b, float c, float d) {
+	if (t == 0) return b;  if ((t /= d) == 1) return b + c;
+	float p = d * .3f;
+	float a = c;
+	float s = p / 4;
+	return max(0,(a * pow(2, -10 * t) * sin((t * d - s) * (2 * M_PI) / p) + c + b));
+}
+
+float ElasticEaseInOut(float t, float b, float c, float d) {
+	if (t == 0) return b;  if ((t /= d / 2) == 2) return b + c;
+	float p = d * (.3f * 1.5f);
+	float a = c;
+	float s = p / 4;
+
+	if (t < 1) {
+		float postFix = a * pow(2, 10 * (t -= 1)); // postIncrement is evil
+		return -.5f * (postFix * sin((t * d - s) * (2 * M_PI) / p)) + b;
+	}
+	float postFix = a * pow(2, -10 * (t -= 1)); // postIncrement is evil
+	return postFix * sin((t * d - s) * (2 * M_PI) / p) * .5f + c + b;
+}
+
 TweenInterpolation TweenInterpolationMethods[INTERP_TYPE_COUNT][EASE_TYPE_COUNT] = {
 	{CubicEaseIn, CubicEaseOut, CubicEaseInOut},
 	{SineEaseIn, SineEaseOut, SineEaseInOut},
 	{CircEaseIn, CircEaseOut, CircEaseInOut},
 	{ExpoEaseIn, ExpoEaseOut, ExpoEaseInOut},
 	{QuadEaseIn, QuadEaseOut, QuadEaseInOut},
+	{ElasticEaseIn, ElasticEaseOut, ElasticEaseInOut}
 };
