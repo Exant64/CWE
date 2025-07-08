1ac67909fac4cf449b93df38fb9a46928d86ce35
Exant64 <warpcauldronproject@gmail.com>
2025-07-08 16:58:13 +0300

    removed bad check for shiny jewel monotones for dnc shiny tex lerp

--- CWE/al_draw.cpp
+++ CWE/al_draw.cpp
@@ -196,7 +196,7 @@ void CWE_ShinyCheck(int shiny)
 void ChaoColoring(int texture, int color, int shiny, int monotone, int shinyJewelMonotone, NJS_CNK_MODEL* model)
 {
 	// if the chao is shiny twotone, or shiny monotone apply the lerp
-	if (!shinyJewelMonotone && (shiny == 2 || (shiny && monotone))) {
+	if (shiny == 2 || (shiny && monotone)) {
 		AL_DayNightCycle_SetLerpShinyTexture();
 	}
 
